126779e31825039b75b8d808b9dc6eadd2bea6d6
kb3120 <kb3120@8BPGRG3>
2022-01-03 18:43:40 +0100

    Add project files.

---
+++ ClinicManager/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClinicManager.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            String message = $"Index Page visited at {DateTime.UtcNow.ToLongTimeString()}";
+            _logger.LogInformation(message);
+        }
+    }
+}
