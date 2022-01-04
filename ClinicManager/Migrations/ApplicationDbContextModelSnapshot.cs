bf2fc0d6fdbc932d56bc66048e524a6aa618c258
Benedikt <benedikt.kuett@gmail.com>
2022-01-04 23:26:22 +0100

    Initial

--- ClinicManager/Migrations/ApplicationDbContextModelSnapshot.cs
+++ ClinicManager/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -4,18 +4,16 @@ using ClinicManager.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace ClinicManager.Data.Migrations
+namespace ClinicManager.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20220104160721_User2")]
-    partial class User2
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
